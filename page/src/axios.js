2625fa7809d8ae08a8c22054797c62e13bc40b21
chendongming0602 <chendm0602@163.com>
2019-07-07 17:15:23 +0800

    build

--- page/src/axios.js
+++ page/src/axios.js
@@ -5,7 +5,7 @@ import axios from 'axios'
 //3.设置跨域请求保护session
 axios.defaults.withCredentials=true;
 //4.设置请求服务器基础路径
-axios.defaults.baseURL="http://127.0.0.1:3000/"
+axios.defaults.baseURL="http://cdmmovie.applinzi.com/"
 //5.将axios注册  Vue示例
 Vue.prototype.axios=axios
 //7.在main.js 引入axios即可
